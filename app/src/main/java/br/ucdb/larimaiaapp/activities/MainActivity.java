34ffb55ba1aa1a09a27e1bc0e5a31b0dce8ff3a7
Marcos3yk <marcos3yk@gmail.com>
2015-09-23 22:29:51 -0400

    Implementação da telas do pedido e dos models

--- app/src/main/java/br/ucdb/larimaiaapp/activities/MainActivity.java
+++ app/src/main/java/br/ucdb/larimaiaapp/activities/MainActivity.java
@@ -4,17 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import br.ucdb.larimaiaapp.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
